f57b644354a15afa4f5d4e86027956b21d8d8818
Karim Gharib <ichkarimgharib2005@gmail.com>
2018-05-15 06:05:29 +0200

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      endDrawer: new Drawer(
+
+      ),
+      appBar: new AppBar(
+        title: new Text("Home"),
+      ),
+      body: new Center(
+        child: new Text("Home Page")
+      )
+    );
+  }
+}
